7b12d7f766ab4927c80e48490b271667e5f5d2bd
Ran Har-Shuv <ran.harshuv@gmail.com>
2018-07-30 10:29:48 +0300

    add the project page

--- server.js
+++ server.js
@@ -57,6 +57,14 @@ app.get( '/about', (req, res) => {
     });
 });
 
+app.get( '/project', (req, res) => {
+    res.render('project.hbs', {
+        pageTitle: 'Project page',
+        currentYear: new Date().getFullYear()
+    });
+});
+
+
 app.get('/bad', (req, res) => {
     res.send({
         errorMessage: 'This is a bad request!'
